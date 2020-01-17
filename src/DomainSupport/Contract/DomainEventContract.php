cc0f5bae9a5b62f0800f5ec3b2d952e68d807d0f
Ivan Novak <ivan@onlinemeded.org>
2020-01-17 08:47:15 -0600

    Renamed EventContract & Added AggregateRootContract

--- src/DomainSupport/Contract/DomainEventContract.php
+++ src/DomainSupport/Contract/DomainEventContract.php
@@ -5,7 +5,7 @@ namespace OnlineMedEd\DomainDrivenLaravel\DomainSupport;
 use DateTimeImmutable;
 use OnlineMedEd\DomainDrivenLaravel\DomainSupport\Contract\EntityIdContract;
 
-interface EventContract
+interface DomainEventContract
 {
     public function __construct(EntityIdContract $id, array $data, ?DateTimeImmutable $dateTime = null);
 }
